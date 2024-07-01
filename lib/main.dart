0049b19ae537bb415e4ac7f8513c729b50a253bf
bewar.gohdar <bewar.00429239@gmail.com>
2024-07-01 03:04:25 +0300

    create appbar

--- lib/main.dart
+++ lib/main.dart
@@ -79,25 +79,21 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      //add line between appbar and body
+
+      body: Column(children: [
+        SizedBox(
+          height: 10,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Container(
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                  color: Color.fromARGB(111, 158, 158, 158), width: 1.0),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
